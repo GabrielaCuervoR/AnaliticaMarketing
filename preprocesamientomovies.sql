7dc22d0809839ab07f59c76a5cb10fa9106f2631
pablitojp1 <115129231+pablitojp1@users.noreply.github.com>
2023-10-02 16:50:16 -0500

    actualizacion preprocesamiento

--- preprocesamientomovies.sql
+++ preprocesamientomovies.sql
@@ -2,36 +2,23 @@
 drop table if exists fecha;
 create table fecha AS
 SELECT timestamp,
-strftime('%Y-%m-%d %H:%M:%S', timestamp, 'unixepoch') AS fecha FROM ratings
+strftime('%Y', timestamp, 'unixepoch') AS fecha FROM ratings;
 
---Unir fecha a tabla ratings
-drop table if exists new_ratings;
-create table new_ratings AS
-SELECT *
-FROM fecha AS f
-LEFT JOIN ratings AS r ON r.timestamp = f.timestamp;
+--Seleccionar usuarios que hayan calificado
+drop table if exists rating_user2;
+create table rating_user2 AS
+select userId as userId,
+count(*) as calificación
+from ratings
+group by userId
+having calificación >=20 and calificación <=600
+order by calificación asc;
 
--- Unir con movies y new_ratings
-drop table if exists base_nueva;
-create table base_nueva AS
-SELECT movies.*,
-new_ratings.userid,
-new_ratings.rating,
-new_ratings.fecha
-FROM movies
-LEFT JOIN new_ratings ON movies.movieId = new_ratings.movieId;
-
-
----crear tabla agrupada por titulo de la pelicula y con rating mayor a 3.5
-
-drop table if exists calificacion;
-create table calificacion as 
-select title AS Titulo,
-rating AS Calificacion
-from base_nueva
-WHERE rating > 3.5
-group by title;
-
-SELECT * FROM calificacion
-
+--Unir usuarios final con ratings original
+drop table if exists rating_final;
+create table ratings_new1 AS
+select a.*
+from ratings a 
+inner join ratings_user2 b
+on a.userId =b.userId;
 
